440872d1ba7f631f1e3283fe0a99065cdc6a2aba
Nina Vergmann <nina152b@kea.stud.dk>
2022-10-12 12:29:42 +0200

    NY MENU SKAL FORBEREDES

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+document
+  .querySelector(".dropbtn_hoved_menu")
+  .addEventListener("click", myFunction);
+
+//Når brugeren klikker på menuen, kommer den frem og den kan toggles væk igen
+function myFunction() {
+  document.getElementById("myDropdownProgram").classList.toggle("show");
+  document.querySelector(".header ul").style.overflow = "visible";
+}
