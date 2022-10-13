ba9eeb56946f92585d0980b94f7852d403a38483
bellakez <bellakezia28@stud.kea.dk>
2022-10-13 16:57:04 +0200

    cookiebox

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import "./style.css";
 
+// COOKIEBOX
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+  //koden nedenunder, gør at boksen aldrig kommer igen, men ved at fjerne kommer den igen ved reload i browser
+  // }
+  // if (sessionStorage.getItem("fjerncookieboks")) {;
+  // cookieboks.classList.add("hidden");
+}
+
 // TIL PROGRAM
 document
   .querySelector(".dropbtn_hoved_menu")
